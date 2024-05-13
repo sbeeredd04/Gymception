c43de6ba47b774d6cf553c12f48f4c6caf35ac15
Sri Ujjwal Reddy Beereddy <113922053+sbeeredd04@users.noreply.github.com>
2024-05-13 05:59:48 +0000

    database url changes

--- gymception/settings.py
+++ gymception/settings.py
@@ -91,9 +91,16 @@ WSGI_APPLICATION = "gymception.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('STACKHERO_MYSQL_DATABASE_URL'), conn_max_age=600)
+    'default': dj_database_url.parse(
+        os.getenv('STACKHERO_MYSQL_DATABASE_URL'),
+        engine='django.db.backends.mysql',  # Specify the backend explicitly if needed
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
